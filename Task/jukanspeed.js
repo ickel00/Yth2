3419fa47eb075ce33a1ddcda2c3474ab3796afa8
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-01-01 00:06:53 +0800

    Update jukanspeed.js

--- Task/jukanspeed.js
+++ Task/jukanspeed.js
@@ -7,7 +7,8 @@
 
 */
 const $ = new Env('聚看点极速')
-
+let drawcash = $.getdata('jukan_cash') || 30 //提现金额
+let wxname = $.getdata('jukan_name') || ""//微信真实名字，可以在双引号内填入
 let CookieArr=[],BodyArr=[];
 let cookie = $.getdata('jukan_ck')
 let bodys = $.getdata('jukan_body')
