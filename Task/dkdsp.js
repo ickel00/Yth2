843622738055db94c8dde5ab744427868afc3bad
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-02-08 14:28:14 +0800

    Update dkdsp.js

--- Task/dkdsp.js
+++ Task/dkdsp.js
@@ -57,7 +57,7 @@ const dkdbody = process.env.dkdbody;
 const dkdhd = process.env.dkdhd;
 const bodys = process.env.dkdvd_body;
 
-let ReadArr = [], YouthBody = "", readscore = 0;
+let ReadArr = [], bodyArr = "", readscore = 0;
 if (!(bodys && bodys != '')) {
   $.msg("", "", '请先刷视频获取多body获取越多，脚本可获得金币越多')
   $.done()
@@ -65,18 +65,18 @@ if (!(bodys && bodys != '')) {
 
 if ($.isNode()) {
   if (process.env.dkdvd_body && process.env.dkdvd_body.indexOf('&') > -1) {
-  YouthBody = bodys.split('&');
+  bodyArr = bodys.split('&');
   } else {
-      YouthBody = bodys.split.split()
+      bodyArr = bodys.split()
   };
  
 }
 
 // YouthBody = bodys.split('&');
 
-Object.keys(YouthBody).forEach((item) => {
-  if (YouthBody[item]) {
-    ReadArr.push(YouthBody[item])
+Object.keys(bodyArr).forEach((item) => {
+  if (bodyArr[item]) {
+    ReadArr.push(bodyArr[item])
   }
 })
 let indexLast = $.getdata('dkdvd_body_index');
