67dddd1fe3ed1995ba04d5990abbb8585f18c77d
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-02-26 18:46:49 +0800

    Update TT.js

--- Task/TT.js
+++ Task/TT.js
@@ -74,6 +74,8 @@ if ($.isNode()) {
 }
 
 if ($.isNode()) {
+    TTreferArr.push('TTrefe')
+    TTbodyArr.push('TTbody')
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  } else {
