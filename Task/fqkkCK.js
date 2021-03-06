1dd7db793710ad1da440082defd300be7bb2432b
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-03-06 23:13:55 +0800

    Add files via upload

---
+++ Task/fqkkCK.js
@@ -0,0 +1,17 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let fqkkVal="[\n  {\n    \"uid\": 4017528,\n    \"url\": \"http://m.cdyiyuan.xyz/reada/getTask\",\n    \"hd\": \"{\\\"Accept\\\":\\\"*/*\\\",\\\"Accept-Encoding\\\":\\\"gzip, deflate\\\",\\\"Origin\\\":\\\"http://m.cdyiyuan.xyz\\\",\\\"Cookie\\\":\\\"autoRead=1; Hm_lpvt_84099950848427564e5e4b4310ad032e=1615000037; Hm_lvt_84099950848427564e5e4b4310ad032e=1615000012; udtauth=eb42DX0uabuD07%2Bqi5XP9bH3yMGlc16JGfk%2BX8vZZcoVuwFcWRgA2bW9fxBmjvgEK%2FsBg9aloN3DWE6rqxVQAGsQPMdr6qPkekQ60DxtDSmEi2VQCRjCf%2FWzpMDgnToBmMdWzmv4BTRSoJNBcN9j5uLmCXFutbUs4hRg2tgCu9Y; PHPSESSID=tid9jt8uianb5e72hmiie3m5c8\\\",\\\"Connection\\\":\\\"keep-alive\\\",\\\"Host\\\":\\\"m.cdyiyuan.xyz\\\",\\\"Content-Length\\\":\\\"0\\\",\\\"User-Agent\\\":\\\"Mozilla/5.0 (iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000232) NetType/WIFI Language/zh_CN\\\",\\\"Referer\\\":\\\"http://m.cdyiyuan.xyz/reada?upuid=3950781\\\",\\\"Accept-Language\\\":\\\"zh-cn\\\",\\\"X-Requested-With\\\":\\\"XMLHttpRequest\\\"}\"\n  },\n  {\n    \"uid\": 4019423,\n    \"url\": \"http://m.acc888.xyz/reada/getTask\",\n    \"hd\": \"{\\\"Accept\\\":\\\"*/*\\\",\\\"Accept-Encoding\\\":\\\"gzip, deflate\\\",\\\"Origin\\\":\\\"http://m.acc888.xyz\\\",\\\"Cookie\\\":\\\"autoRead=1; Hm_lpvt_84099950848427564e5e4b4310ad032e=1615009444; Hm_lvt_84099950848427564e5e4b4310ad032e=1615009409,1615009444; udtauth=3ae9sEIQqLaApa0v4%2BHYLo4FthDRlDnu2nKEu94GxIKUA2A4Ydcj5ns5fuS1Es6w%2BjeJi3aosRKArd%2BCFECo%2FeuiseWSWcPZT3FOgiIrn5pxVJRUjFa3x2lM7CzWLindcdt0YMf2RqnqmwUrU8od%2FKcP7rKticRepH%2FX%2BrhueeI; PHPSESSID=kastk2e190vapc84bin7cg56fd\\\",\\\"Connection\\\":\\\"keep-alive\\\",\\\"Host\\\":\\\"m.acc888.xyz\\\",\\\"Content-Length\\\":\\\"0\\\",\\\"User-Agent\\\":\\\"Mozilla/5.0 (iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000233) NetType/WIFI Language/zh_CN\\\",\\\"Referer\\\":\\\"http://m.acc888.xyz/reada?upuid=4017528\\\",\\\"Accept-Language\\\":\\\"zh-cn\\\",\\\"X-Requested-With\\\":\\\"XMLHttpRequest\\\"}\"\n  }\n]"
+
+
+let fqkkck = {
+
+  fqkkVal: fqkkVal,  
+  
+}
+
+module.exports = fqkkck
+  
+
+
