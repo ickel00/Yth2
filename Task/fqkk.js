76505cb7ce077b9b061677f29e870f8f2b62fc60
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-03-06 12:09:59 +0800

    Update fqkk.js

--- Task/fqkk.js
+++ Task/fqkk.js
@@ -168,7 +168,7 @@ function execTask(ac, i) {
 async function fqkkck() {
   if ($request.url.indexOf("getTask") > -1) {
     const fqkkurl = $request.url;
-    const fqkkhd = JSON.stringify(fqkkhd);
+    const fqkkhd = JSON.stringify($request.headers);
     let host = fqkkurl.match(/http?:\/\/(.+?)\//)[1];
     let ck = $request.headers['Cookie'] || $request.headers['cookie'];
     let [userId, gold] = await userInfo(host, ck);
