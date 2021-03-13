b2681b87ec81fc8ce82967dbb737b2de4b268aeb
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-03-13 10:19:01 +0800

    Add files via upload

--- Task/hldsp.js
+++ Task/hldsp.js
@@ -177,10 +177,10 @@ console.log('\n哈喽短视频[领取视频红包]回执:失败🚫')
 function hldspqd(timeout = 0) {
   return new Promise((resolve) => {
     setTimeout( ()=>{
-      if (ishldsphd = typeof $.getdata('hldsphd') === "undefined") {
-        $.msg($.name,"",'请先获取哈喽短视频数据!😓',)
-        $.done()
-      }
+      //if (typeof $.getdata('hldsphd') === "undefined") {
+        //$.msg($.name,"",'请先获取哈喽短视频数据!😓',)
+        //$.done()
+      //}
 hlsign = hldspurl.match(/sign=(\w+)/)[1]
 hluid = hldspurl.match(/uid=(\w.+)/)[1]
 
